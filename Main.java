6255ae277bc0cdc2443450a8cbd65afaa44aa534
Kaito-Fujie <m1m02530153@oit.ac.jp>
2025-06-23 11:13:51 +0900

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
-  }
+    public static void main(String[] args) {
+        System.out.println("SEMONSTER GAME");
+        System.out.println("Show draw monsters!");
+
+        Player player = new Player();          // Playerオブジェクトの作成
+        player.drawMonsters();                 // モンスターを引く
+        player.showDeck();                     // デッキの内容を表示
+    }
 }
